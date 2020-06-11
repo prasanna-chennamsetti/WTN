8a470eee9b3b06511325a8e7e6c1ee5ece45ff76
Maha <Maha@LAPTOP-LM9NFPOD>
2020-06-11 09:54:34 +0530

    first exercise

--- MileStone1/src/com/wipro/languagebasics/Exercise1.java
+++ MileStone1/src/com/wipro/languagebasics/Exercise1.java
@@ -2,7 +2,6 @@ package com.wipro.languagebasics;
 
 public class Exercise1 {
 public static void main(String[] args) {
-	int a=Integer.parseInt(args[0]);
-	System.out.println(a);
+	System.out.println(args[0]+" Technologies "+args[1]);
 }
 }
