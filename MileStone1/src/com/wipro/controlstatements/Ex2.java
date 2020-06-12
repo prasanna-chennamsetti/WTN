be1723ec0bf4e4b451b8fc5a2b835e5294150aac
Maha <Maha@LAPTOP-LM9NFPOD>
2020-06-12 07:31:20 +0530

    Exercise 2 of flowcontrol statements updated

---
+++ MileStone1/src/com/wipro/controlstatements/Ex2.java
@@ -0,0 +1,14 @@
+package com.wipro.controlstatements;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		if(a%2==0) {
+			System.out.println("Even");
+		}
+		else {
+			System.out.println("Odd");
+		}
+	}
+
+}
