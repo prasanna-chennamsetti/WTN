20797aee93315b9a98b838e56e229e3960392997
Maha <Maha@LAPTOP-LM9NFPOD>
2020-06-12 08:57:48 +0530

    Exercise 7 of flow control statements

---
+++ MileStone1/src/com/wipro/controlstatements/Ex7.java
@@ -0,0 +1,14 @@
+package com.wipro.controlstatements;
+
+public class Ex7 {
+	public static void main(String[] args) {
+		char s='G';
+		if(s>='a' && s<='z') {
+			System.out.println(Character.toUpperCase(s));
+		}
+		else {
+			System.out.println(Character.toLowerCase(s));
+		}
+	}
+
+}
