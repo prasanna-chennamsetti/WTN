be1e3d5a4f40c988c344567e3e71777443dcf58f
Maha <Maha@LAPTOP-LM9NFPOD>
2020-06-12 09:08:06 +0530

    Exercise 8 of flow control statements

---
+++ MileStone1/src/com/wipro/controlstatements/Ex8.java
@@ -0,0 +1,30 @@
+package com.wipro.controlstatements;
+
+public class Ex8 {
+	public static void main(String[] args) {
+		String s=args[0];
+		switch(s) {
+		case "R":
+			System.out.println("Red");
+			break;
+		case "B":
+			System.out.println("Blue");
+			break;
+		case "G":
+			System.out.println("Green");
+			break;
+		case "O":
+			System.out.println("Orange");
+			break;
+		case "Y":
+			System.out.println("Yellow");
+			break;
+		case "W":
+			System.out.println("White");
+			break;
+		default:
+			System.out.println("Invalid code");
+		}
+	}
+
+}
