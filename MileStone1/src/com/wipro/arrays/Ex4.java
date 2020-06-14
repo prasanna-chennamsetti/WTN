9a04ea30506b808a953d102373750dbd136a46c1
Maha <Maha@LAPTOP-LM9NFPOD>
2020-06-14 19:49:48 +0530

    Exercise 4 of arrays

---
+++ MileStone1/src/com/wipro/arrays/Ex4.java
@@ -0,0 +1,13 @@
+package com.wipro.arrays;
+
+public class Ex4 {
+	public static void main(String[] args) {
+		int a[]={66,72};
+		char c;
+		for(int i=0;i<a.length;i++) {
+			c=(char)a[i];
+			System.out.print(c+" ");
+		}
+	}
+
+}
