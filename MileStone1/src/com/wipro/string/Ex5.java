64d88b11e9a5c95e51c091267dcb07bd8c1cad94
Maha <Maha@LAPTOP-LM9NFPOD>
2020-07-01 07:24:32 +0530

    Ex5 of strings

---
+++ MileStone1/src/com/wipro/string/Ex5.java
@@ -0,0 +1,9 @@
+package com.wipro.string;
+import java.util.*;
+public class Ex5 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		System.out.println(s.substring(1,s.length()-1));
+	}
+}
