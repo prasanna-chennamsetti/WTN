89235c915e8a555ed2845c786992437b11e0afc5
Ch.Prasanna <170031562@kluniversity.in>
2020-08-01 14:18:29 +0530

    Ex4 of Collections

---
+++ Milestone2/src/com/wipro/collections/Ex4.java
@@ -0,0 +1,32 @@
+package com.wipro.collections;
+
+import java.util.*;
+
+public class Ex4 {
+	public static void main(String[] args) {
+		List<Object> list = new MyArrayList<>();
+		
+		try {
+			list.add(15);
+			list.add(1.2F);
+			list.add(3.1415D);
+			list.add("Try");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(list);
+
+	}
+}
+class MyArrayList<E> extends ArrayList<E> {
+	public boolean add(E e) {
+		if (e instanceof Integer || e instanceof Float || e instanceof Double) {
+			super.add(e);
+			return true;
+		} else {
+			throw new ClassCastException("Only Integer, Float, and Double are allowed");
+		}
+	}
+}
+
